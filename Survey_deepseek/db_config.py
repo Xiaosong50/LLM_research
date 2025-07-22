6d0b39f187106158e9ba9a1b197491242bf9d3e8
Xiaosong Lu <xlu50@hawk.iit.edu>
2025-07-22 10:26:16 +0800

    config

--- Survey_deepseek/db_config.py
+++ Survey_deepseek/db_config.py
@@ -2,12 +2,12 @@ import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        # host='localhost',
-        # user='admin',
-        # password='Lxs,123321',
-        # database='survey'
         host='localhost',
         user='admin',
-        password='admin123321',
-        database='llm_survey'
+        password='Lxs,123321',
+        database='survey'
+        # host='localhost',
+        # user='admin',
+        # password='admin123321',
+        # database='llm_survey'
     )
